b3703b36b5a60a17af56ba59e7ce8daaff7dafd5
junhwan <oprt12@gmail.com>
2013-04-30 21:31:55 +0900

    no message

--- src/com/hipits/fishingalarm/activity/MainActivity.java
+++ src/com/hipits/fishingalarm/activity/MainActivity.java
@@ -20,7 +20,6 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.Window;
 import android.widget.EditText;
 import android.widget.Toast;
 
